d05f97f7c6e46c820ed4e0b558d68524007d90ee
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-04-25 16:41:01 +0100

    Added `instruction_startswith`

--- Debugger/external_libraries/nmd_assembly_wrapper.cpp
+++ Debugger/external_libraries/nmd_assembly_wrapper.cpp
@@ -2,7 +2,13 @@
 #define NMD_ASSEMBLY_IMPLEMENTATION
 #include <nmd_assembly/nmd_assembly.h>
 
-void dissassemble_x86_32(const unsigned char* buffer, const unsigned char* buffer_end, int(print_func)(const char*, ...))
+#ifdef _WIN64
+const NMD_X86_MODE mode = NMD_X86_MODE_64;
+#else
+const NMD_X86_MODE mode = NMD_X86_MODE_32;
+#endif
+
+void dissassemble_x86(const unsigned char* buffer, const unsigned char* buffer_end, int(print_func)(const char*, ...))
 {
 	if (!buffer || !buffer_end || !print_func)
 		return;
@@ -12,7 +18,7 @@ void dissassemble_x86_32(const unsigned char* buffer, const unsigned char* buffe
 
 	for (size_t buffer_offset = 0; buffer_offset < size_t(buffer_end - buffer); buffer_offset += instruction.length)
 	{
-		if (!nmd_x86_decode(buffer + buffer_offset, buffer_end - (buffer + buffer_offset), &instruction, NMD_X86_MODE_32, NMD_X86_DECODER_FLAGS_MINIMAL))
+		if (!nmd_x86_decode(buffer + buffer_offset, buffer_end - (buffer + buffer_offset), &instruction, mode, NMD_X86_DECODER_FLAGS_MINIMAL))
 			break;
 
 		nmd_x86_format(&instruction, formatted_instruction, NMD_X86_INVALID_RUNTIME_ADDRESS, NMD_X86_FORMAT_FLAGS_DEFAULT);
@@ -21,21 +27,23 @@ void dissassemble_x86_32(const unsigned char* buffer, const unsigned char* buffe
 	}
 }
 
-void dissassemble_x86_64(const unsigned char* buffer, const unsigned char* buffer_end, int(print_func)(const char*, ...))
+bool instruction_startswith(const unsigned char* buffer, const unsigned char* buffer_end, const char* search)
 {
-	if (!buffer || !buffer_end || !print_func)
-		return;
+	if (!buffer || !buffer_end || !search)
+		return false;
 
 	nmd_x86_instruction instruction;
 	char formatted_instruction[128];
 
 	for (size_t buffer_offset = 0; buffer_offset < size_t(buffer_end - buffer); buffer_offset += instruction.length)
 	{
-		if (!nmd_x86_decode(buffer + buffer_offset, buffer_end - (buffer + buffer_offset), &instruction, NMD_X86_MODE_64, NMD_X86_DECODER_FLAGS_MINIMAL))
+		if (!nmd_x86_decode(buffer + buffer_offset, buffer_end - (buffer + buffer_offset), &instruction, mode, NMD_X86_DECODER_FLAGS_MINIMAL))
 			break;
 
 		nmd_x86_format(&instruction, formatted_instruction, NMD_X86_INVALID_RUNTIME_ADDRESS, NMD_X86_FORMAT_FLAGS_DEFAULT);
 
-		print_func("%s\n", formatted_instruction);
+		return _nmd_strstr(formatted_instruction, search) == formatted_instruction;
 	}
+
+	return false;
 }
--- Debugger/external_libraries/nmd_assembly_wrapper.h
+++ Debugger/external_libraries/nmd_assembly_wrapper.h
@@ -1,4 +1,5 @@
 #pragma once
 
-void dissassemble_x86_32(const unsigned char* buffer, const unsigned char* buffer_end, int(print_func)(const char*, ...));
-void dissassemble_x86_64(const unsigned char* buffer, const unsigned char* buffer_end, int(print_func)(const char*, ...));
+void dissassemble_x86(const unsigned char* buffer, const unsigned char* buffer_end, int(print_func)(const char*, ...));
+
+bool instruction_startswith(const unsigned char* buffer, const unsigned char* buffer_end, const char* _instruction);
