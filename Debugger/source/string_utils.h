4e8570da9c5a49a07f0037132c0ba93b33ef91b2
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-05-08 09:24:23 +0100

    Added `find_pattern`

--- Debugger/source/string_utils.h
+++ Debugger/source/string_utils.h
@@ -17,3 +17,24 @@ void cswcsncpy(wchar_t* dest, size_t size_in_bytes, const wchar_t* src, size_t m
 
 bool string_starts(const char* s1, const char* s2);
 bool string_ends(const char* s1, const char* s2);
+
+// based off https://github.com/B1nGh0/FindPattern/blob/709154d5bf0d6d31944c5df7f3d5c79cc87a7d2f/findpattern.c
+template<unsigned long k_size, unsigned long k_length>
+unsigned char* find_pattern(unsigned char(&start)[k_size], const unsigned char(&pattern)[k_length], const char(&mask)[k_length + 1])
+{
+	unsigned long i = 0, j = 0;
+	unsigned char fnd = 0;
+
+	if (k_length >= k_size)
+		return 0;
+
+	for (i = 0; !fnd && i <= k_size - k_length; i++)
+	{
+		for (fnd = 1, j = 0; fnd && j < k_length; j++)
+		{
+			fnd &= mask[j] == '?' || pattern[j] == (start)[i + j];
+		}
+	}
+
+	return fnd ? start + i - 1 : nullptr;
+}
