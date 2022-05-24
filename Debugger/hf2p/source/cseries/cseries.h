55558e396468db81768565c2cf99a0926f9bc9cf
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-05-24 07:15:26 +0100

    Added `c_flags`

---
+++ Debugger/hf2p/source/cseries/cseries.h
@@ -0,0 +1,20 @@
+#pragma once
+
+TODO("find the actual place this is stored")
+template<typename t_enum, typename t_storage, t_enum k_count>
+class c_flags
+{
+public:
+	c_flags()
+	{
+	}
+
+	inline void set(t_enum value)
+	{
+		if (value < k_count)
+			m_value |= (1 << (t_storage)value);
+	}
+
+protected:
+	t_storage m_value;
+};
