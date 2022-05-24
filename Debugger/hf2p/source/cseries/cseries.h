937b6bea886c650586ad748954e79f5c896db28c
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-05-24 09:41:44 +0100

    Added `c_enum`

--- Debugger/hf2p/source/cseries/cseries.h
+++ Debugger/hf2p/source/cseries/cseries.h
@@ -18,3 +18,22 @@ public:
 protected:
 	t_storage m_value;
 };
+
+TODO("find the actual place this is stored")
+template<typename t_enum, typename t_storage, t_enum k_count>
+class c_enum
+{
+public:
+	c_enum()
+	{
+	}
+
+	inline void set(t_enum value)
+	{
+		if (value < k_count)
+			m_value = (t_storage)value;
+	}
+
+protected:
+	t_storage m_value;
+};
